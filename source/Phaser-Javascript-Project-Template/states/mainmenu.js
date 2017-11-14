ea21723fa2b968ccdbeef0cf43adccc1432c7b10
bennyboss <hubbub@hotmail.com>
2017-11-14 13:57:05 +0100

    Updated the schermen text file, set a background image for the mainmenu

--- source/Phaser-Javascript-Project-Template/states/mainmenu.js
+++ source/Phaser-Javascript-Project-Template/states/mainmenu.js
@@ -2,6 +2,7 @@
     
 };
 
+var mainMenuBackground;
 var createGameBtn;
 var gameInstructionsBtn;
 var creditsBtn;
@@ -14,7 +15,8 @@ GameStates.MainMenu.prototype = {
         //this.loadingText = this.add.text(this.game.width / 2, this.game.height / 2, "Press Enter to start", { font: "20px monospace", fill: "#fff" });
         //this.loadingText.anchor.setTo(0.5, 0.5);
 
-
+        mainMenuBackground = this.add.sprite(this.world.centerX, this.world.centerY, 'mainmenu-background');
+        mainMenuBackground.anchor.setTo(0.5, 0.5);
         createGameBtn = this.add.button(this.world.centerX, this.world.centerY, 'createGameBtn', this.createGameBtnOnClick, this);
         gameInstructionsBtn = this.add.button(this.world.centerX, this.world.centerY, 'gameInstructionsBtn', this.createGameInstructionsBtnOnClick, this);
         creditsBtn = this.add.button(this.world.centerX, this.world.centerY, 'creditsBtn', this.createCreditsBtnOnClick, this);
