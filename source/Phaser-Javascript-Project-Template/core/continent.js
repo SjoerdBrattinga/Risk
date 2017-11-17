60851efa332e538530440d6f24c31abd09f809cd
bennyboss <hubbub@hotmail.com>
2017-11-17 10:00:49 +0100

    Added a continent js file

--- source/Phaser-Javascript-Project-Template/core/continent.js
+++ source/Phaser-Javascript-Project-Template/core/continent.js
@@ -1,1 +1,33 @@
-﻿
+﻿function Continent (name, extraArmies, territoriesInContinet)
+{
+    var name;
+    var extraArmies;
+    var territoriesInContinet;
+    var borderTerritories;
+    var color;
+};
+
+function isOwned()
+{
+  //code...
+};
+
+function getOwned()
+{
+    //code...
+};
+
+function getTerritoriesInContinet()
+{
+    //code...
+};
+
+function getBorderTerritories()
+{
+    //code...
+};
+
+function setColor(color)
+{
+    //code...
+}
