d46fa66fdbe164012d5ec9a1ab6b75c00ed3461a
SjoerdBrattinga <sjoerd-j-b@hotmail.com>
2017-11-17 09:50:03 +0100

    territory.js added

--- source/Phaser-Javascript-Project-Template/core/territory.js
+++ source/Phaser-Javascript-Project-Template/core/territory.js
@@ -1,1 +1,37 @@
-﻿
+﻿function Territory(name, positionX, positionY) {
+    this.name = name;
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.owner = {};
+    this.armies = 0;
+    this.color = 0;
+    this.borderTerritories = [];
+}
+
+function setOwner(player) {
+    this.owner = player;
+}
+
+function getOwner() {
+    return this.owner;
+}
+
+function getNumberOfArmies() {
+    return this.armies;
+}
+
+function addArmies(armiesToAdd) {
+    this.armies += armiesToAdd;
+}
+
+function removeArmies(armiesToRemove) {
+    this.armies -= armiesToRemove;
+}
+
+function getBorderTerritories() {
+    return this.borderTerritories;
+}
+
+function setBorderTerritories(borderTerritories) {
+    this.borderTerritories = borderTerritories;
+}
