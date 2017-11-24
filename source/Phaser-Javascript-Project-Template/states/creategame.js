d74c3b8aa96c1f7ebead0bb90a4ea788b699ab10
bennyboss <hubbub@hotmail.com>
2017-11-24 12:29:03 +0100

    Couldnt get a working input field added what I have so far as comments

--- source/Phaser-Javascript-Project-Template/states/creategame.js
+++ source/Phaser-Javascript-Project-Template/states/creategame.js
@@ -5,6 +5,7 @@
 var text;
 var startGameBtn;
 var addPlayerBtn;
+//var input;
 
 GameStates.CreateGame.prototype = {
     create: function () {
@@ -13,6 +14,12 @@ GameStates.CreateGame.prototype = {
         startGameBtn = this.add.button(this.world.centerX, this.world.centerY, 'startGameBtn', this.startGameOnClick, this);
         addPlayerBtn = this.add.button(this.world.centerX, this.world.centerY, 'addPlayerBtn', this.addPlayerOnClick, this);
         startGameBtn.anchor.setTo(0.7, 0.2);
+        //Trying to get a text input field
+        //debugger;
+        //input = this.add.inputField(10,90);
+        // input = new CanvasInput({
+        //     canvas: document.getElementById('canvas')
+        // });
     },
 
     startGameOnClick: function () {
@@ -22,6 +29,10 @@ GameStates.CreateGame.prototype = {
     addPlayerOnClick: function () {
         addPlayer(0, 'sjors', 'yellow');
     },
+
+    // update: function()  {
+    //     this._inputField.update();
+    // },
 
 
 };
