b130ea4db3d80a3f0a548c74754fe684566d1e73
bennyboss <hubbub@hotmail.com>
2017-12-21 15:59:18 +0100

    Text now full in phaser when adding player and remove button now next to player name

--- source/Phaser-Javascript-Project-Template/states/creategame.js
+++ source/Phaser-Javascript-Project-Template/states/creategame.js
@@ -22,6 +22,23 @@ GameStates.CreateGame.prototype = {
         //removePlayerBtn = this.add.button(this.world.centerX, this.world.centerY, 'removePlayerBtn', this.removePlayerOnClick, this);
         startGameBtn.anchor.setTo(0.7, 0.2);
         addPlayerBtn.anchor.setTo(0.7, 2);
+
+
+        var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+        player1 = this.game.add.text(0,0," ", style);
+        player2 = this.game.add.text(0,0," ",style);
+        player3 = this.game.add.text(0,0," ",style);
+        player4 = this.game.add.text(0,0," ", style);
+        player5 = this.game.add.text(0,0," ",style);
+        player6 = this.game.add.text(0,0," ",style);
+        //message.setShadow(3,3, 'rgba(0,0,0,0,5)', 2);
+        player1.setTextBounds(0, 100, 600, 100);
+        player2.setTextBounds(0, 150, 600, 100);
+        player3.setTextBounds(0, 200, 600, 100);
+        player4.setTextBounds(0, 250, 600, 100);
+        player5.setTextBounds(0, 300, 600, 100);
+        player6.setTextBounds(0, 350, 600, 100);
+        //player1.setBackgroundColor(3,3, 'rgba(255,255,255)', 2);
         //removePlayerBtn.anchor.setTo(3.5, 2);
 
         // var bar = this.add.graphics();
@@ -77,6 +94,54 @@ GameStates.CreateGame.prototype = {
             value = validateNameInput(nameInput);
             if (value) {
                 var player = addPlayer(0, value, color);
+                if(players.length === 1){
+                    player1.setText(player.name + " " + player.color);
+                    removePlayerBtn = this.add.button(this.world.centerX, this.world.centerY*players.length/2, 'removePlayerBtn', function(){
+                        removePlayer(player.name);
+                        if(players.length < GameStates.MAX_PLAYERS)
+                            addPlayerBtn.visible = true;
+                    }, this);
+                }
+                if(players.length == 2){
+                    player2.setText(player.name + " " + player.color);
+                    removePlayerBtn = this.add.button(this.world.centerX, this.world.centerY*players.length/2.8, 'removePlayerBtn', function(){
+                        removePlayer(player.name);
+                        if(players.length < GameStates.MAX_PLAYERS)
+                            addPlayerBtn.visible = true;
+                    }, this);
+                }
+                if(players.length == 3){
+                    player3.setText(player.name + " " + player.color);
+                    removePlayerBtn = this.add.button(this.world.centerX, this.world.centerY*players.length/3.3, 'removePlayerBtn', function(){
+                        removePlayer(player.name);
+                        if(players.length < GameStates.MAX_PLAYERS)
+                            addPlayerBtn.visible = true;
+                    }, this);
+                }
+                if(players.length == 4){
+                    player4.setText(player.name + " " + player.color);
+                    removePlayerBtn = this.add.button(this.world.centerX, this.world.centerY*players.length/3.7, 'removePlayerBtn', function(){
+                        removePlayer(player.name);
+                        if(players.length < GameStates.MAX_PLAYERS)
+                            addPlayerBtn.visible = true;
+                    }, this);
+                }
+                if(players.length == 5){
+                    player5.setText(player.name + " " + player.color);
+                    removePlayerBtn = this.add.button(this.world.centerX, this.world.centerY*players.length/3.9, 'removePlayerBtn', function(){
+                        removePlayer(player.name);
+                        if(players.length < GameStates.MAX_PLAYERS)
+                            addPlayerBtn.visible = true;
+                    }, this);
+                }
+                if(players.length == 6){
+                    player6.setText(player.name + " " + player.color);
+                    removePlayerBtn = this.add.button(this.world.centerX, this.world.centerY*players.length/4.0, 'removePlayerBtn', function(){
+                        removePlayer(player.name);
+                        if(players.length < GameStates.MAX_PLAYERS)
+                            addPlayerBtn.visible = true;
+                    }, this);
+                }
                 console.log(player, 'added');
                 console.log(players);
                 if (players.length === GameStates.MAX_PLAYERS) {
@@ -92,11 +157,10 @@ GameStates.CreateGame.prototype = {
         message = document.getElementById("overview");
         console.log(players);
         var haiku = "Turtles and mushrooms\nYou are in the wrong castle\nFireball mustache";
-        var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
-        message = this.game.add.text(0,0,player.name + " " + player.color +  "\n", style);
-        message.setShadow(3,3, 'rgba(0,0,0,0,5)', 2);
-        message.setTextBounds(0, 100, 600, 100);
-
+        // var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+        // message = this.game.add.text(0,0,"TEKST ", style);
+        // message.setShadow(3,3, 'rgba(0,0,0,0,5)', 2);
+        // message.setTextBounds(0, 100, 600, 100);
         var bar = this.game.add.graphics();
         bar.beginFill(0x000000, 0.2);
         bar.drawRect(0, 100, 800, 100);
@@ -109,15 +173,7 @@ GameStates.CreateGame.prototype = {
         // text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
 
         //  We'll set the bounds to be from x0, y100 and be 800px wide by 100px high
-        text.setTextBounds(0, 100, 800, 100);
-
-        removePlayerBtn = this.add.button(this.world.centerX, this.world.centerY*players.length/2, 'removePlayerBtn', function(){
-            removePlayer(player.name);
-            if(players.length < GameStates.MAX_PLAYERS)
-                addPlayerBtn.visible = true;
-        }, this);
-        removePlayerBtn.anchor.setTo(3, 5.8);
-
+        //text.setTextBounds(0, 100, 800, 100);
     },
 
     removePlayerOnClick: function(name) {
