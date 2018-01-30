050a7a836e0aa45166b8cae579b6ecc523bd81f0
bennyboss <hubbub@hotmail.com>
2018-01-30 12:55:28 +0100

    Endscreen now has proper x and y positions and instructions screen actually contains instructions

--- source/Phaser-Javascript-Project-Template/states/endscreen.js
+++ source/Phaser-Javascript-Project-Template/states/endscreen.js
@@ -2,22 +2,23 @@ GameStates.EndScreen = function (game) {
 
 };
 
-var text;
-var text;
-var continueBtn;
-
 GameStates.EndScreen.prototype = {
     create: function () {
 
         var backGround = this.add.sprite(this.world.centerX, this.world.centerY, 'creategame-background');
         backGround.anchor.setTo(0.5, 0.5);
 
-        text = this.add.text(100, 20, 'VICTORY! ', {
+        var text = this.add.text(this.world.centerX, 70, 'VICTORY!', {
             fontSize: '150px', fill: currentPlayer.color});
-        text = this.add.text(110, 180, currentPlayer.name + ' conquered Leeuwarden!', {
+        text.anchor.setTo(0.5);
+        var text2 = this.add.text(this.world.centerX, 180, currentPlayer.name + ' conquered Leeuwarden!', {
             fontSize: '40px', fill: currentPlayer.color});
-        continueBtn = this.add.button(765, 304, 'continueBtn', this.returnOnClick, this);
+        text2.anchor.setTo(0.5);
+        var continueBtn = this.add.button(765, 304, 'continueBtn', this.returnOnClick, this);
         continueBtn.anchor.setTo(0.5);
+
+        $('#form1').hide();
+        $('#form2').hide();
     },
 
     returnOnClick: function () {
--- source/Phaser-Javascript-Project-Template/states/instructions.js
+++ source/Phaser-Javascript-Project-Template/states/instructions.js
@@ -9,8 +9,16 @@ GameStates.Instructions.prototype = {
     create: function () {
         text = this.add.text(100, 100, 'Instructions on how the game is played:', {
             fontSize: '32px', fill: '#fff'});
-        backBtn = this.add.button(this.world.centerX, this.world.centerY, 'backBtn', this.returnOnClick, this);
-        backBtn.anchor.setTo(-2, 0.2);
+        text = this.add.text(100, 150, 'First we place armies on territories.', {
+            fontSize: '25px', fill: '#fff'});
+        text = this.add.text(100, 200, 'Now we\'re ready to attack!', {
+            fontSize: '25px', fill: '#fff'});
+        text = this.add.text(100, 250, 'Last but not least, we need to make sure', {
+            fontSize: '25px', fill: '#fff'});
+        text = this.add.text(100, 275,  'our outer territories are fortified', {
+            fontSize: '25px', fill: '#fff'});
+        backBtn = this.add.button(765, 304, 'backBtn', this.returnOnClick, this);
+        backBtn.anchor.setTo(0.5);
     },
 
     returnOnClick: function () {
